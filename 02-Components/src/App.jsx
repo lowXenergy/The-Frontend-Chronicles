3a00e55ac58c0b3b9b7daddd3c6fbcffb8032500
D Max <dmax@Ds-MacBook-Air.local>
2026-02-20 12:12:06 +0530

    Added 02-Components folder

---
+++ 02-Components/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './index.css';
+import Card from './component/Card'
+import Navbar from './component/Navbar';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Navbar/>
+      <Card/>
+      <Card/>
+    </div>
+  )
+}
+
+export default App
